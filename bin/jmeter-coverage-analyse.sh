70451a9782425adf16ac3c05ba86b6f3d3ad2b00
李焕忠 <562462@hopson.com.cn>
2022-03-08 10:18:42 +0800

    完善分析脚本

--- bin/jmeter-coverage-analyse.sh
+++ bin/jmeter-coverage-analyse.sh
@@ -1,40 +1,64 @@
 #!/bin/bash 
-set -v on
+# set -v on
+
+export LC_COLLATE='C'
+export LC_CTYPE='C'
+
+# 处理路径中存在.jmx导致后续处理把文件夹也作为文件处理并报错的问题
+if [ -d "../src/test/jmeter/script/hilife-clothingsuppl.jmx" ]; then
+    mv ../src/test/jmeter/script/hilife-clothingsuppl.jmx ../src/test/jmeter/script/hilife-clothingsuppl
+fi
+
+# 处理文件名中存在空格的文件，这种文件在命令行中导致xargs命令处理参数是会出错
+if [ -f "../src/test/jmeter/script/transactionmanage/hilife-Template/apiTest_ template.jmx" ]; then
+    mv "../src/test/jmeter/script/transactionmanage/hilife-Template/apiTest_ template.jmx" "../src/test/jmeter/script/transactionmanage/hilife-Template/apiTest_template.jmx"
+fi
+
+if [ -e "../out" ]; then
+    rm -rf ../out
+fi
+
+if [ ! -d "../out" ]; then
+    mkdir ../out
+fi
+if [ ! -d "../out/tmp" ]; then
+    mkdir ../out/tmp
+fi
 
 echo 开始处理Jmeter文件
-find ../ | grep .jmx | xargs grep "HTTPSampler.path" > ../out/interfacelist
+find ../ | grep .jmx | xargs grep "HTTPSampler.path" > ../out/tmp/interfacelist
 echo 去掉xml标签头
-sed "s/\<stringProp\ name=\"HTTPSampler.path\"\>//g" ../out/interfacelist > ../out/interfacelist1
+sed "s/\<stringProp\ name=\"HTTPSampler.path\"\>//g" ../out/tmp/interfacelist > ../out/tmp/interfacelist1
 echo 去掉xml标签尾
-sed "s/\<\/stringProp\>/\ /g" ../out/interfacelist1 > ../out/interfacelist2
+sed "s/\<\/stringProp\>/\ /g" ../out/tmp/interfacelist1 > ../out/tmp/interfacelist2
 echo 去掉前面文件路径部分
 # (.*)标识匹配出换行符之外的任意字符任意次
-sed "s/\.\.\/\/src\/test\/jmeter.*\.jmx\://g" ../out/interfacelist2 > ../out/interfacelist3
+sed "s/\.\.\/\/src\/test\/jmeter.*\.jmx\://g" ../out/tmp/interfacelist2 > ../out/tmp/interfacelist3
 echo 去头尾空格
-sed 's/[[:space:]]//g' ../out/interfacelist3 > ../out/interfacelist4
+sed 's/[[:space:]]//g' ../out/tmp/interfacelist3 > ../out/tmp/interfacelist4
 echo 去掉带有？的？后面的部分
-sed 's/\?.*$//g' ../out/interfacelist4 > ../out/interfacelist5
+sed 's/\?.*$//g' ../out/tmp/interfacelist4 > ../out/tmp/interfacelist5
 echo 去掉带协议和domain部分
-sed 's/https:\/\/test\.91hiwork\.com//g' ../out/interfacelist5 > ../out/interfacelist6
+sed 's/https:\/\/test\.91hiwork\.com//g' ../out/tmp/interfacelist5 > ../out/tmp/interfacelist6
 echo 去掉带协议和domain部分
-sed 's/https:\/\/token\.91hiwork\.com//g' ../out/interfacelist6 > ../out/interfacelist7
+sed 's/https:\/\/token\.91hiwork\.com//g' ../out/tmp/interfacelist6 > ../out/tmp/interfacelist7
 echo 去掉带协议和domain部分
-sed 's/https:\/\/token\.91hework\.com//g' ../out/interfacelist7 > ../out/interfacelist8
-sed '/^\s*$/d' ../out/interfacelist8 > ../out/interfacelist9
+sed 's/https:\/\/token\.91hework\.com//g' ../out/tmp/interfacelist7 > ../out/tmp/interfacelist8
+sed '/^\s*$/d' ../out/tmp/interfacelist8 > ../out/tmp/interfacelist9
 echo 确保每行都已/开头，并去掉uri中存在的//的情况
 echo 1
 # 在每一行开头添加/
-sed "s/^/\//g" ../out/interfacelist9 > ../out/interfacelist10
+sed "s/^/\//g" ../out/tmp/interfacelist9 > ../out/tmp/interfacelist10
 echo 2
 # 将///替换成/
-sed 's/\/\/\//\//g' ../out/interfacelist10 > ../out/interfacelist11
+sed 's/\/\/\//\//g' ../out/tmp/interfacelist10 > ../out/tmp/interfacelist11
 echo 3
 # 将//替换成/
-sed 's/\/\//\//g' ../out/interfacelist11 > ../out/interfacelist12
+sed 's/\/\//\//g' ../out/tmp/interfacelist11 > ../out/tmp/interfacelist12
 
 
 echo 去重
-sort ../out/interfacelist12 | uniq > ../out/interfacelist13
+sort ../out/tmp/interfacelist12 | uniq > ../out/tmp/interfacelist13
 
 
 # 按项目处理jmeter接口中多余的部分
@@ -113,35 +137,69 @@ my_array=(
 for(( i=0;i<${#my_array[@]};i++)) do
     let interfacelistsource=i+13
     let interfacelistresult=i+14
-    echo ../out/interfacelist${interfacelistsource}
-    echo ../out/interfacelist${interfacelistresult}
+    # echo ../out/interfacelist${interfacelistsource}
+    # echo ../out/interfacelist${interfacelistresult}
     keystr=${my_array[$i]}
-    echo ${keystr}
-    echo ${my_array[$i]}
-    echo 's/^\/'${keystr}'\//\//g'
-    sed 's/^\/'${keystr}'\//\//g' ../out/interfacelist${interfacelistsource} > ../out/interfacelist${interfacelistresult}
+    # echo ${keystr}
+    # echo ${my_array[$i]}
+    # echo 's/^\/'${keystr}'\//\//g'
+    sed 's/^\/'${keystr}'\//\//g' ../out/tmp/interfacelist${interfacelistsource} > ../out/tmp/interfacelist${interfacelistresult}
 done;
+let latestIndex=${#my_array[*]}+13
+echo ${latestIndex}
+sort ../out/tmp/interfacelist${latestIndex} | uniq > ../out/jmeter-interfacelist
 #######################################################################################################################
 
 echo 开始处理yapi接口导出文件
-grep "        \"path\": \"" ../yapi/api.json > ../out/yapi-interfacelist
+grep "        \"path\": \"" ../yapi/api.json > ../out/tmp/yapi-interfacelist0
 echo 去头尾空格
-sed 's/[[:space:]]//g' ../out/yapi-interfacelist > ../out/yapi-interfacelist1
+sed 's/[[:space:]]//g' ../out/tmp/yapi-interfacelist0 > ../out/tmp/yapi-interfacelist1
 echo 去重
-sort ../out/yapi-interfacelist1 | uniq > ../out/yapi-interfacelist2
+sort ../out/tmp/yapi-interfacelist1 | uniq > ../out/tmp/yapi-interfacelist2
 echo 双斜线替换成单斜线
-sed 's/\/\//\//g' ../out/yapi-interfacelist2 > ../out/yapi-interfacelist3
+sed 's/\/\//\//g' ../out/tmp/yapi-interfacelist2 > ../out/tmp/yapi-interfacelist3
 echo 去掉无用的头部
-sed 's/\"path\":\"//g' ../out/yapi-interfacelist3 > ../out/yapi-interfacelist4
+sed 's/\"path\":\"//g' ../out/tmp/yapi-interfacelist3 > ../out/tmp/yapi-interfacelist4
 echo 去掉无用的尾部
-sed 's/\"\,//g' ../out/yapi-interfacelist4 > ../out/yapi-interfacelist5
+sed 's/\"\,//g' ../out/tmp/yapi-interfacelist4 > ../out/yapi-interfacelist
 
 #######################################################################################################################
 # 开始计算覆盖率
 echo 合并文件
-cat ../out/interfacelist82 ../out/yapi-interfacelist5 > ../out/merge
+cat ../out/jmeter-interfacelist ../out/yapi-interfacelist > ../out/tmp/merge
 
-echo 取重复项
-sort ../out/merge | uniq -d > ../out/coverage-interface-list
-echo 取非重复项
-sort ../out/merge | uniq  > ../out/not-coverage-interface-list
+echo 取重复项（jmeter覆盖到的yapi接口清单）
+sort ../out/tmp/merge | uniq -d > ../out/jmeter-match-in-yapi
+
+echo 输出在Jmeter中有但没有在yapi中记录的接口
+cat ../out/jmeter-interfacelist ../out/jmeter-match-in-yapi > ../out/tmp/merge2
+sort ../out/tmp/merge2 | uniq -u > ../out/jmeter-not-match-in-yapi
+
+echo 输出在Yapi中但没有在Jmeter中的接口
+cat ../out/yapi-interfacelist ../out/jmeter-match-in-yapi > ../out/tmp/merge3
+sort ../out/tmp/merge3 | uniq -u > ../out/yapi-not-match-in-jmeter
+
+
+# awk '{print NR}' ../out/jmeter-interfacelist | tail -n1
+# awk '{print NR}' ../out/jmeter-match-in-yapi | tail -n1
+# awk '{print NR}' ../out/jmeter-not-match-in-yapi | tail -n1
+# awk '{print NR}' ../out/yapi-not-match-in-jmeter | tail -n1
+
+jmeter_interfacelist=$(wc -l < ../out/jmeter-interfacelist)
+echo "Jmeter接口数,${jmeter_interfacelist}" >> ../out/converage-result.csv
+
+yapi_interfacelist=$(wc -l < ../out/yapi-interfacelist)
+echo "Yapi接口数,${yapi_interfacelist}"  >> ../out/converage-result.csv
+
+jmeter_match_in_yapi=$(wc -l < ../out/jmeter-match-in-yapi)
+echo "Jmeter与Yapi匹配上的接口数,${jmeter_match_in_yapi}"  >> ../out/converage-result.csv
+
+jmeter_not_match_in_yapi=$(wc -l < ../out/jmeter-not-match-in-yapi)
+echo "Jmeter中未与Yapi匹配的接口数,${jmeter_not_match_in_yapi}" >> ../out/converage-result.csv
+
+yapi_not_match_in_jmeter=$(wc -l < ../out/yapi-not-match-in-jmeter)
+echo "yapi中未与Jmeter汽配的接口数,${yapi_not_match_in_jmeter}" >> ../out/converage-result.csv
+
+export jmeter_match_in_yapi;
+export yapi_interfacelist
+awk  'BEGIN{printf "整体代码覆盖率，%0.2f",ENVIRON["jmeter_match_in_yapi"]/ENVIRON["yapi_interfacelist"]*100}' >> ../out/converage-result.csv
