e972614d6f68d75b04d844523936ac670b3e1c93
李焕忠 <562462@hopson.com.cn>
2022-05-14 15:44:35 +0800

    添加注释

--- bin/yapi-auto-gen-jmeter.sh
+++ bin/yapi-auto-gen-jmeter.sh
@@ -4,6 +4,9 @@ if [ ! -d "../out/auto-gen-jmeter" ]; then
     mkdir -p ../out/auto-gen-jmeter
 fi
 
+# API定义文件./template/test/yapi-auto-jmeter-demo.json
+# 生成Jmeter脚本的模板./template/jmeter-template
+# 输出目录../out/auto-gen-jmeter/yapi-auto-jmeter-demo
 java -jar ../lib/openapi-generator-cli.jar \
     generate -i ./template/test/yapi-auto-jmeter-demo.json \
         -t ./template/jmeter-template \
