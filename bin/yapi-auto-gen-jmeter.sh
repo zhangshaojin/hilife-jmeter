08613d2de47911a4b7208ae9875586232c7ae431
李焕忠 <562462@hopson.com.cn>
2022-05-14 15:42:56 +0800

    修改脚本，并提交测试使用的api定义文件

--- bin/yapi-auto-gen-jmeter.sh
+++ bin/yapi-auto-gen-jmeter.sh
@@ -4,31 +4,8 @@ if [ ! -d "../out/auto-gen-jmeter" ]; then
     mkdir -p ../out/auto-gen-jmeter
 fi
 
-
-# java -jar ../lib/openapi-generator-cli.jar \
-#     generate -i ../out/yapi-tmp/hilife-vshop-44-swaggerapi.json -o ../out/auto-gen-jmeter/hilife-vshop-44 -g mysql-schema \
-#     --skip-validate-spec
-
-# java -jar ../lib/openapi-generator-cli.jar \
-#     generate -i ../out/yapi-tmp/hilife-vshop-44-swaggerapi.json -o ../out/auto-gen-jmeter/hilife-vshop-44/objc -g objc \
-#     --skip-validate-spec
-
-
-# java -jar ../lib/openapi-generator-cli.jar \
-#     generate -i ../out/yapi-tmp/yapidoc-analyes-366-swaggerApi.json -o ../out/auto-gen-jmeter/yapidoc-analyes-366 -g jmeter \
-#     --skip-validate-spec
-
-# java -jar ../lib/openapi-generator-cli.jar \
-#     generate -i ../out/yapi-tmp/hilife-yhbill-consumer-yapidoc.json -o ../out/auto-gen-jmeter/hilife-yhbill-consumer-yapidoc -g jmeter \
-#     --skip-validate-spec
-
-# java -jar ../lib/openapi-generator-cli.jar \
-#     generate -i ../out/yapi-tmp/yapidoc-analyes-366-vshop-swaggerApi.json -o ../out/auto-gen-jmeter/yapidoc-analyes-366-vshop -g jmeter \
-#     --skip-validate-spec -t ./template/jmeter-template
-
 java -jar ../lib/openapi-generator-cli.jar \
-    generate -i ../out/yapi-tmp/yapi-auto-jmeter-demo.json -o ../out/auto-gen-jmeter/yapi-auto-jmeter-demo -g jmeter \
-    --skip-validate-spec -t ./template/jmeter-template
-
-
-# jq ../out/yapi-tmp/yapidoc-analyes-366-swaggerApi.json
+    generate -i ./template/test/yapi-auto-jmeter-demo.json \
+        -t ./template/jmeter-template \
+        -o ../out/auto-gen-jmeter/yapi-auto-jmeter-demo -g jmeter \
+        --skip-validate-spec
