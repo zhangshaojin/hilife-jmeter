bc478f2a4c5a2e0bfa783fe8010580aa904979ef
李焕忠 <562462@hopson.com.cn>
2022-05-18 18:06:01 +0800

    修改注释

--- bin/yapi-auto-gen-jmeter.sh
+++ bin/yapi-auto-gen-jmeter.sh
@@ -8,7 +8,7 @@ if [ ! -d "../out/auto-gen-jmeter" ]; then
     mkdir -p ../out/auto-gen-jmeter
 fi
 
-# API定义文件./template/test/yapi-auto-jmeter-demo.json
+# API定义文件./template/test/demo-yapidoc-1.json；./template/test/demo-yapidoc-2.json
 #   API定义文件暂定使用yapidoc-maven-plugin导入yapi server时生成的定义文件
 #   注：不使用yapi server导出的Swagger文件的主要原因是yapi server导出的文件缺失components部分
 # 生成Jmeter脚本的模板./template/jmeter-template
