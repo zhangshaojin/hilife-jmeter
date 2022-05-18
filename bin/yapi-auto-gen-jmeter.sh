a6c3e5d7a4ba402d1377a94b5353b725c1f953f1
李焕忠 <562462@hopson.com.cn>
2022-05-18 10:56:22 +0800

    修改api.mustache支持post body参数生成

--- bin/yapi-auto-gen-jmeter.sh
+++ bin/yapi-auto-gen-jmeter.sh
@@ -5,10 +5,14 @@ if [ ! -d "../out/auto-gen-jmeter" ]; then
 fi
 
 # API定义文件./template/test/yapi-auto-jmeter-demo.json
+#   API定义文件暂定使用yapidoc-maven-plugin导入yapi server时生成的定义文件
+#   注：不使用yapi server导出的Swagger文件的主要原因是yapi server导出的文件缺失components部分
 # 生成Jmeter脚本的模板./template/jmeter-template
+#   该模板中根据合生活现有接口情况做了一些定制
 # 输出目录../out/auto-gen-jmeter/yapi-auto-jmeter-demo
 java -jar ../lib/openapi-generator-cli.jar \
-    generate -i ./template/test/yapi-auto-jmeter-demo.json \
+    generate -i ./template/test/hilife-crm-backapi-yapidoc-1.json \
         -t ./template/jmeter-template \
-        -o ../out/auto-gen-jmeter/yapi-auto-jmeter-demo -g jmeter \
+        -o ../out/auto-gen-jmeter/hilife-crm-backapi-yapidoc-1 -g jmeter \
         --skip-validate-spec
+        
